c8bf0b2d1b28d0ab132769138116c25a506e4b9e
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-12 21:26:05 +0000

    using callbacks when fetching info from api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,25 @@
 const request = require('request');
-const breedId = process.argv[2];
-console.log();
-//const httpLink = 'https://api.thecatapi.com/v1/images/search?breed_id=beng'
-const httpLink = `https://api.thecatapi.com/v1/images/search?breed_id=${breedId}`;
-console.log(httpLink);
+//const httpLink = `https://api.thecatapi.com/v1/images/search?breed_id=${breedName}`;
 
-request(httpLink, (error, _response, body) => {
+const fetchBreedDescription = function (breedName, callback) {
+  const breedInitals = breedName.toLowerCase().slice(0, 3)
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedInitals}`, (error, _response, body) => {
 
-  console.log(body.length);
-  if (error) {
-    console.log('URL does not exist and/or is invalid');
-    return;
-  } else if (body.length === 2) {
-    console.log('Breed request does not exist and/or is invalid');
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0]);
-//  console.log(typeof data);
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback(error, null);
+    }
+    const desc = data[0].description.trim();
+  
+  callback(null, desc); //is this right?
 });
+};
 
 
+module.exports = { fetchBreedDescription };
 
 
 //sudo command
@@ -28,4 +27,4 @@ request(httpLink, (error, _response, body) => {
 //2. touch .gitignore node_modules
 //3. Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
 //3.a request from endpoint
-// 4. content is a string => convert js object
+//4. content is a string => convert js object
---
+++ index.js
@@ -0,0 +1,15 @@
+//This file will be the one that users can run and provide the breed name to.
+//It will then require the breedFetcher file and call its exported function.
+
+const { fetchBreedDescription } = require('./breedFetcher');
+const breedName = process.argv[2];
+
+
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc); 
+  }
+});
